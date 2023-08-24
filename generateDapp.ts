15bd31b3076a6f16c125a054ce752bce4fe08274
maayan <maayansavir@gmail.com>
2023-08-24 15:24:16 -0700

    revert to use git clone

--- generateDapp.ts
+++ generateDapp.ts
@@ -1,7 +1,5 @@
 import { execSync } from "child_process";
-import os from "os";
 import chalk from "chalk";
-
 import {
   ARGUMENT_NAMES,
   Template,
@@ -25,79 +23,40 @@ export const generateDapp = async (opts: {
   network: Network;
   packageManager: PackageManager;
 }) => {
-  const platform = os.platform();
-  console.log("platform", platform);
-  const projectName = opts[ARGUMENT_NAMES.NAME] || "my-aptos-dapp";
-  let templateDirectory;
+  const repoName = opts[ARGUMENT_NAMES.NAME] || "my-aptos-dapp";
+  let repoAddr;
   switch (opts[ARGUMENT_NAMES.TEMPLATE]) {
     case "todolist":
-      templateDirectory = "todolist";
+      repoAddr = "https://github.com/aptos-labs/cad-todolist.git";
       break;
-    case "dapp-boilerplate":
-      templateDirectory = "dapp-boilerplate";
-      break;
-    case "node-boilerplate":
-      templateDirectory = "node-boilerplate";
+    case "new":
+      repoAddr = "https://github.com/aptos-labs/cad-boilerplate.git";
       break;
     default:
-      templateDirectory = "dapp-boilerplate";
+      repoAddr = "https://github.com/aptos-labs/cad-boilerplate.git";
   }
+  const gitCheckoutCommand = `git clone ${repoAddr} ${repoName}`;
+  const deleteDotGitCommand = `rm -rf ${repoName}/.git`;
+  const installRootDepsCommand = `cd ${repoName} && ${opts.packageManager} install`;
+  const replaceNpmUsagesCommand = `cd ${repoName} && sed -i.bak 's/npm/${opts.packageManager}/g' package.json && rm package.json.bak`;
 
-  let copyTemplateToProjectDirectory;
-  let installRootDepsCommand;
-  let replaceNpmUsagesCommand;
-
-  if (platform === "win32") {
-    copyTemplateToProjectDirectory = `xcopy /s /e templates\\${templateDirectory} ${projectName}\\`;
-    installRootDepsCommand = `cd ${projectName} && ${opts.packageManager} install`;
-    replaceNpmUsagesCommand = `cd ${projectName} && (Get-Content package.json).replace('npm', '${opts.packageManager}') | Set-Content package.json`;
-  } else {
-    copyTemplateToProjectDirectory = `cp -r templates/${templateDirectory} ${projectName}/`;
-    installRootDepsCommand = `cd ${projectName} && ${opts.packageManager} install`;
-    replaceNpmUsagesCommand = `cd ${projectName} && sed -i.bak 's/npm/${opts.packageManager}/g' package.json && rm package.json.bak`;
-  }
-
-  // Creating project directory
-  console.log("Creating project...");
-  const copiedTemplate = runCommand(copyTemplateToProjectDirectory);
-  if (!copiedTemplate) {
-    console.error("Failed to create project directory");
+  // Clone the repo
+  console.log("Cloning template repo...");
+  const checkedOut = runCommand(gitCheckoutCommand);
+  const deleteDotGit = runCommand(deleteDotGitCommand);
+  if (!checkedOut || !deleteDotGit) {
+    console.error("Failed to clone the repo");
     process.exit(-1);
   } else {
-    console.log("Created successfully");
+    console.log("Cloned successfully");
   }
 
   // create .env file
   console.log("Creating .env file...");
-  if (platform == "win32") {
-    runCommand(`cd ${projectName} && echo. > .env`);
-  } else {
-    runCommand(`cd ${projectName} && touch .env`);
-  }
+  runCommand(`cd ${repoName} && touch .env`);
   const network = opts[ARGUMENT_NAMES.NETWORK] || "testnet";
-
-  // TODO more sophisticate way to distinguish between node and web env
-  if (platform == "win32") {
-    if (templateDirectory === "node-boilerplate") {
-      execSync(`echo APP_NETWORK=${network} > ${projectName}\\.env`);
-      execSync(`echo APP_NETWORK=${network} > ${projectName}\\node\\.env`);
-    } else {
-      execSync(`echo VITE_APP_NETWORK=${network} > ${projectName}\\.env`);
-      execSync(
-        `echo VITE_APP_NETWORK=${network} > ${projectName}\\frontend\\.env`
-      );
-    }
-  } else {
-    if (templateDirectory === "node-boilerplate") {
-      runCommand(`echo "APP_NETWORK=${network}" > ${projectName}/.env`);
-      runCommand(`echo "APP_NETWORK=${network}" > ${projectName}/node/.env`);
-    } else {
-      runCommand(`echo "VITE_APP_NETWORK=${network}" > ${projectName}/.env`);
-      runCommand(
-        `echo "VITE_APP_NETWORK=${network}" > ${projectName}/frontend/.env`
-      );
-    }
-  }
+  runCommand(`echo "VITE_APP_NETWORK=${network}" > ${repoName}/.env`);
+  runCommand(`echo "VITE_APP_NETWORK=${network}" > ${repoName}/frontend/.env`);
 
   // Install dependencies
   console.log("Installing dependencies...");
@@ -113,11 +72,11 @@ export const generateDapp = async (opts: {
 
   console.log(chalk.bold("\nNext steps:") + "\n");
   console.log(
-    chalk.green(`1. run [cd ${projectName}] to your dapp directory.`) + "\n"
+    chalk.green(`1. run [cd ${repoName}] to your dapp directory.`) + "\n"
   );
   console.log(
     chalk.green(
-      `2. run [${opts.packageManager} run move:init] to initialize a new Profile.`
+      `2. run [${opts.packageManager} run move:init] to initialize a new CLI Profile.`
     ) + "\n"
   );
   console.log(
@@ -130,7 +89,6 @@ export const generateDapp = async (opts: {
       `4. run [${opts.packageManager} run move:publish] to publish your contract.`
     ) + "\n"
   );
-
   console.log(
     chalk.green(`5. run [${opts.packageManager} start] to run your dapp.`) +
       "\n"
