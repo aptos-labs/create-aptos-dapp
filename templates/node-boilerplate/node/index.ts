becd5d64115e1311ec8fb1493070cdb63f4b171f
maayan <maayansavir@gmail.com>
2023-09-11 19:18:06 +0800

    add license prop in package json file

--- templates/node-boilerplate/node/index.ts
+++ templates/node-boilerplate/node/index.ts
@@ -1,5 +1,6 @@
 const main = async () => {
-  console.log(`Start my Aptos dapp`);
+  console.log("This dapp was scaffold by create-apt-dapp");
+  console.log("Edit node/index.ts to start your project");
 };
 
 main().catch(console.error);
