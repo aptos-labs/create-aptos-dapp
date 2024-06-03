b538102f26b9f729e390ae3cd3935958c9fd9a9b
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-06-03 22:24:20 +0800

    remove next app

--- templates/nft-template/nextapp/src/utils/math.ts
+++
@@ -1,7 +0,0 @@
-export const onChainToHumanReadable = (value: number, decimal: number) => {
-  return (value / Math.pow(10, decimal)).toFixed(decimal);
-};
-
-export const humanReadableToOnChain = (value: number, decimal: number) => {
-  return value * Math.pow(10, decimal);
-};
