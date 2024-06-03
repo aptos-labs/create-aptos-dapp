1cc04cc3d049ad2355e2e4ff9e54f2b76cbc1723
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-06-03 22:28:43 +0800

    nit

--- templates/nft-template/scripts/move/test.js
+++ templates/nft-template/scripts/move/test.js
@@ -5,7 +5,7 @@ const cli = require("@aptos-labs/ts-sdk/dist/common/cli/index.js");
 
 const config = yaml.load(fs.readFileSync("./.aptos/config.yaml", "utf8"));
 const accountAddress =
-  config["profiles"]["testnet-profile-1"];
+  config["profiles"][process.env.VITE_APP_NETWORK]["account"];
 async function test() {
   const move = new cli.Move();
 
