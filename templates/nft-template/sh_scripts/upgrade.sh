03ef900c9993b3341b70939820235943c2f9c435
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-06-04 23:47:37 +0800

    fix upgrade script

--- templates/nft-template/sh_scripts/upgrade.sh
+++ templates/nft-template/sh_scripts/upgrade.sh
@@ -9,10 +9,12 @@ echo "##### Upgrade module #####"
 PUBLISHER_PROFILE=testnet-profile-1
 
 CONTRACT_ADDRESS=$(cat contract_address.txt)
+MINTER_ADDR="0x9d7365d7a09ee3a5610a2131d6ee395531d581e7a7c42582de51a3f111534bbd"
 
 aptos move upgrade-object-package \
+  --skip-fetch-latest-git-deps \
   --package-dir move \
   --object-address $CONTRACT_ADDRESS \
-  --named-addresses launchpad_addr=$CONTRACT_ADDRESS \
+  --named-addresses "launchpad_addr=$CONTRACT_ADDRESS,minter=$MINTER_ADDR"\
   --profile $PUBLISHER_PROFILE \
   --assume-yes
