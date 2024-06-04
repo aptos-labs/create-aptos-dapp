2b6599d829c02528d120ac59f681b88098eacb55
Rasa Welcher <kwelch12@gmail.com>
2024-06-04 13:59:10 -0400

    Initializes shadcn

--- templates/fungible-asset-template/frontend/vite.config.ts
+++ templates/fungible-asset-template/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     open: true,
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
