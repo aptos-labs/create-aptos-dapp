3839f0b2966bbf23ff7574d3d0144ea0d3a23d11
maayan <maayansavir@gmail.com>
2024-06-05 17:17:42 -0400

    refactor template folder structure

--- templates/digital-asset-template/tailwind.config.js
+++ templates/digital-asset-template/tailwind.config.js
@@ -3,12 +3,7 @@ const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./frontend/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
