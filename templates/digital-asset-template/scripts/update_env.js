62a8455b7135d11b4c8fe425df77a0e124c5d9fe
maayan <maayansavir@gmail.com>
2024-06-06 15:53:46 -0400

    override VITE_MODULE_ADDRESS variable if exists

--- templates/digital-asset-template/scripts/update_env.js
+++ templates/digital-asset-template/scripts/update_env.js
@@ -6,5 +6,26 @@ const config = yaml.load(fs.readFileSync("./.aptos/config.yaml", "utf8"));
 const accountAddress =
   config["profiles"][process.env.VITE_APP_NETWORK]["account"];
 
-fs.appendFileSync(".env", `\nVITE_MODULE_ADDRESS=0x${accountAddress}`);
-fs.appendFileSync("frontend/.env", `\nVITE_MODULE_ADDRESS=0x${accountAddress}`);
+const filePath = ".env";
+let envContent = "";
+
+// Check .env file exists and read it
+if (fs.existsSync(filePath)) {
+  envContent = fs.readFileSync(filePath, "utf8");
+}
+
+// Regular expression to match the VITE_MODULE_ADDRESS variable
+const regex = /^VITE_MODULE_ADDRESS=.*$/m;
+const newEntry = `VITE_MODULE_ADDRESS=0x${accountAddress}`;
+
+// Check if VITE_MODULE_ADDRESS is already defined
+if (envContent.match(regex)) {
+  // If the variable exists, replace it with the new value
+  envContent = envContent.replace(regex, newEntry);
+} else {
+  // If the variable does not exist, append it
+  envContent += `\n${newEntry}`;
+}
+
+// Write the updated content back to the .env file
+fs.writeFileSync(filePath, envContent, "utf8");
