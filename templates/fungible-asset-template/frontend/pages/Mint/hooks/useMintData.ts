bf74c11e444433be2a692656dc016cac1eb01df7
Rasa Welcher <kwelch12@gmail.com>
2024-06-13 14:39:43 -0400

    Adds back error handling

--- templates/fungible-asset-template/frontend/pages/Mint/hooks/useMintData.ts
+++ templates/fungible-asset-template/frontend/pages/Mint/hooks/useMintData.ts
@@ -39,16 +39,20 @@ interface MintData {
 }
 
 async function getMintLimit(asset_id: string): Promise<number> {
-  const mintLimitRes = await aptosClient().view<[{ vec: [string] }]>({
-    payload: {
-      function: `${AccountAddress.from(
-        MODULE_ADDRESS
-      )}::launchpad::get_mint_limit`,
-      functionArguments: [asset_id],
-    },
-  });
+  try {
+    const mintLimitRes = await aptosClient().view<[{ vec: [string] }]>({
+      payload: {
+        function: `${AccountAddress.from(
+          MODULE_ADDRESS
+        )}::launchpad::get_mint_limit`,
+        functionArguments: [asset_id],
+      },
+    });
 
-  return Number(mintLimitRes[0].vec[0]);
+    return Number(mintLimitRes[0].vec[0]);
+  } catch (error) {
+    return 0;
+  }
 }
 
 export function useMintData(asset_id: string = config.asset_id) {
@@ -58,15 +62,16 @@ export function useMintData(asset_id: string = config.asset_id) {
     queryKey: ["app-state", asset_id],
     refetchInterval: 1000 * 30,
     queryFn: async () => {
-      if (!asset_id) return null;
+      try {
+        if (!asset_id) return null;
 
-      const res = await aptosClient().queryIndexer<MintQueryResult>({
-        query: {
-          variables: {
-            asset_id,
-            account: account?.address.toString() ?? "",
-          },
-          query: `
+        const res = await aptosClient().queryIndexer<MintQueryResult>({
+          query: {
+            variables: {
+              asset_id,
+              account: account?.address.toString() ?? "",
+            },
+            query: `
             query FungibleQuery($asset_id: String, $account: String) {
               fungible_asset_metadata(where: {asset_type: {_eq: $asset_id}}) {
                 maximum_v2
@@ -93,34 +98,37 @@ export function useMintData(asset_id: string = config.asset_id) {
                 amount
               }
             }`,
-        },
-      });
+          },
+        });
 
-      const asset = res.fungible_asset_metadata[0];
-      if (!asset) return null;
+        const asset = res.fungible_asset_metadata[0];
+        if (!asset) return null;
 
-      return {
-        asset,
-        maxSupply: convertAmountFromOnChainToHumanReadable(
-          asset.maximum_v2 ?? 0,
-          asset.decimals
-        ),
-        currentSupply: convertAmountFromOnChainToHumanReadable(
-          asset.supply_v2 ?? 0,
-          asset.decimals
-        ),
-        uniqueHolders:
-          res.current_fungible_asset_balances_aggregate.aggregate.count ?? 0,
-        totalAbleToMint: convertAmountFromOnChainToHumanReadable(
-          await getMintLimit(asset_id),
-          asset.decimals
-        ),
-        yourBalance: convertAmountFromOnChainToHumanReadable(
-          res.current_fungible_asset_balances[0]?.amount ?? 0,
-          asset.decimals
-        ),
-        isMintActive: asset.maximum_v2 > asset.supply_v2,
-      } satisfies MintData;
+        return {
+          asset,
+          maxSupply: convertAmountFromOnChainToHumanReadable(
+            asset.maximum_v2 ?? 0,
+            asset.decimals
+          ),
+          currentSupply: convertAmountFromOnChainToHumanReadable(
+            asset.supply_v2 ?? 0,
+            asset.decimals
+          ),
+          uniqueHolders:
+            res.current_fungible_asset_balances_aggregate.aggregate.count ?? 0,
+          totalAbleToMint: convertAmountFromOnChainToHumanReadable(
+            await getMintLimit(asset_id),
+            asset.decimals
+          ),
+          yourBalance: convertAmountFromOnChainToHumanReadable(
+            res.current_fungible_asset_balances[0]?.amount ?? 0,
+            asset.decimals
+          ),
+          isMintActive: asset.maximum_v2 > asset.supply_v2,
+        } satisfies MintData;
+      } catch (error) {
+        return null;
+      }
     },
   });
 }
