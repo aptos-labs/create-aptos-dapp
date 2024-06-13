63a5afbdaa1d55084c0131754b669fc27e4f9a2b
maayan <maayansavir@gmail.com>
2024-06-13 12:45:29 -0400

    Add spinner to the wizard flow

--- src/generateDapp.ts
+++ src/generateDapp.ts
@@ -1,13 +1,26 @@
 import { green, bold, white } from "kolorist";
 import path from "path";
 import { fileURLToPath } from "node:url";
-import fs from "node:fs";
+import fs from "fs/promises";
+import type { Ora } from "ora";
 import ora from "ora";
-
+import { exec } from "child_process";
 import { Selections } from "./types.js";
-import { copy, runCommand } from "./utils/helpers.js";
+import { copy } from "./utils/helpers.js";
 
-const spinner = (text: string) => ora({ text, stream: process.stdout });
+const spinner = (text) => ora({ text, stream: process.stdout });
+let currentSpinner: Ora | null = null;
+
+const runCommand = (command) =>
+  new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(stdout ? stdout : stderr);
+    });
+  });
 
 export async function generateDapp(selection: Selections) {
   const projectName = selection.projectName || "my-aptos-dapp";
@@ -18,8 +31,6 @@ export async function generateDapp(selection: Selections) {
     "../../templates",
     selection.template
   );
-  // internal template directory files
-  const files = fs.readdirSync(templateDir);
 
   // current working directory
   const cwd = process.cwd();
@@ -28,85 +39,103 @@ export async function generateDapp(selection: Selections) {
   const targetDirectory = path.join(cwd, projectName);
 
   console.log(); // print new line
-  const toolSpinner = spinner(
+  const scaffoldingSpinner = spinner(
     `Scaffolding project in ${targetDirectory}`
   ).start();
+  currentSpinner = scaffoldingSpinner;
 
-  // make target directory if not exist
-  if (!fs.existsSync(targetDirectory)) {
-    fs.mkdirSync(targetDirectory, { recursive: true });
+  try {
+    // make target directory if not exist
+    await fs.mkdir(targetDirectory, { recursive: true });
+
+    // internal template directory files
+    const files = await fs.readdir(templateDir);
+
+    // write to file
+    const write = async (file: string, content?: string) => {
+      // file to copy to target directory
+      const targetPath = path.join(targetDirectory, file);
+      if (content) {
+        await fs.writeFile(targetPath, content);
+      } else {
+        await copy(path.join(templateDir, file), targetPath);
+      }
+    };
+
+    // loop over template files and write to target directory
+    // ignore .DS_Store, node_modules, package-lock.json, .aptos, build, .env
+    await Promise.all(
+      files
+        .filter(
+          (f) =>
+            f !== ".DS_Store" &&
+            f !== "node_modules" &&
+            f !== "package-lock.json" &&
+            f !== ".aptos" &&
+            f !== "build" &&
+            f !== ".env"
+        )
+        .map((file) => write(file))
+    );
+
+    // cd into target directory
+    process.chdir(targetDirectory);
+
+    scaffoldingSpinner.succeed();
+
+    const npmSpinner = spinner("Installing dependencies.....").start();
+
+    // install dependencies
+    const installRootDepsCommand = `npm install --silent --no-progress`;
+    await runCommand(installRootDepsCommand);
+
+    npmSpinner.succeed();
+    currentSpinner = npmSpinner;
+
+    // create .env file
+    const network = selection.network || "testnet";
+    await write(".env", `VITE_APP_NETWORK=${network}`);
+
+    // Log next steps
+    console.log(
+      green("\nSuccess! You're ready to start building your dapp on Aptos.")
+    );
+
+    console.log(bold("\nNext steps:") + "\n");
+    console.log(
+      green(`1. run ${white(`cd ${projectName}`)} to your dapp directory.`) +
+        "\n"
+    );
+    console.log(
+      green(
+        `2. run ${white("npm run move:init")} to initialize a new CLI Profile.`
+      ) + "\n"
+    );
+    console.log(
+      green(
+        `2. run ${white("npm run move:test")} to run move module unit tests.`
+      ) + "\n"
+    );
+    console.log(
+      green(
+        `3. run ${white("npm run move:compile")} to compile your move contract.`
+      ) + "\n"
+    );
+    console.log(
+      green(
+        `4. run ${white("npm run move:publish")} to publish your contract.`
+      ) + "\n"
+    );
+    console.log(
+      green(`5. run ${white("npm run dev")} to run your dapp.`) + "\n"
+    );
+
+    console.log(
+      green(`6. open up your project in your favorite IDE and start coding!`) +
+        "\n"
+    );
+  } catch (error: any) {
+    currentSpinner?.fail(`Failed to scaffold project: ${error.message}`);
+    console.error(error);
   }
-
-  // write to file
-  const write = (file: string, content?: string) => {
-    // file to copy to target directory
-    const targetPath = path.join(targetDirectory, file);
-    if (content) {
-      fs.writeFileSync(targetPath, content);
-    } else {
-      copy(path.join(templateDir, file), targetPath);
-    }
-  };
-
-  // loop over template files and write to target directory
-  // ignore .DS_Store, node_modules, package-lock.json, .aptos, build, .env
-  for (const file of files.filter(
-    (f) =>
-      f !== ".DS_Store" &&
-      f !== "node_modules" &&
-      f !== "package-lock.json" &&
-      f !== ".aptos" &&
-      f !== "build" &&
-      f !== ".env"
-  )) {
-    write(file);
-  }
-
-  // cd into target directory
-  process.chdir(targetDirectory);
-
-  // install dependencies
-  const installRootDepsCommand = `npm install --silent --no-progress`;
-  runCommand(installRootDepsCommand);
-
-  // create .env file
-  const network = selection.network || "testnet";
-  write(".env", `VITE_APP_NETWORK=${network}`);
-
-  toolSpinner.succeed();
-
-  // Log next steps
-  console.log(
-    green("\nSuccess! You're ready to start building your dapp on Aptos.")
-  );
-
-  console.log(bold("\nNext steps:") + "\n");
-  console.log(
-    green(`1. run ${white(`cd ${projectName}`)} to your dapp directory.`) + "\n"
-  );
-  console.log(
-    green(
-      `2. run ${white("npm run move:init")} to initialize a new CLI Profile.`
-    ) + "\n"
-  );
-  console.log(
-    green(
-      `2. run ${white("npm run move:test")} to run move module unit tests.`
-    ) + "\n"
-  );
-  console.log(
-    green(
-      `3. run ${white("npm run move:compile")} to compile your move contract.`
-    ) + "\n"
-  );
-  console.log(
-    green(`4. run ${white("npm run move:publish")} to publish your contract.`) +
-      "\n"
-  );
-  console.log(green(`5. run ${white("npm run dev")} to run your dapp.`) + "\n");
-
-  console.log(
-    green(`6. open up your project in your favorite IDE and start coding!`) +
-      "\n"
-  );
 }
