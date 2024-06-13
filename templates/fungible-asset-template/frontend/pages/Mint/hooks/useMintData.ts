4c93e421bbfede9273824f9cbf69715db03f07c0
Rasa Welcher <kwelch12@gmail.com>
2024-06-13 14:02:10 -0400

    Fixes big numbers on mint page for fungible assets

--- templates/fungible-asset-template/frontend/pages/Mint/hooks/useMintData.ts
+++ templates/fungible-asset-template/frontend/pages/Mint/hooks/useMintData.ts
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { AccountAddress } from "@aptos-labs/ts-sdk";
 import { MODULE_ADDRESS } from "@/constants";
+import { convertAmountFromOnChainToHumanReadable } from "@/utils/helpers";
 
 export interface FungibleAsset {
   maximum_v2: number;
@@ -55,18 +56,16 @@ export function useMintData(asset_id: string = config.asset_id) {
   return useQuery({
     queryKey: ["app-state", asset_id],
     refetchInterval: 1000 * 30,
-    enabled: !!account,
     queryFn: async () => {
-      try {
-        if (!asset_id) return null;
+      if (!asset_id) return null;
 
-        const res = await aptosClient().queryIndexer<MintQueryResult>({
-          query: {
-            variables: {
-              asset_id,
-              account: account?.address.toString(),
-            },
-            query: `
+      const res = await aptosClient().queryIndexer<MintQueryResult>({
+        query: {
+          variables: {
+            asset_id,
+            account: account?.address.toString() ?? "",
+          },
+          query: `
             query FungibleQuery($asset_id: String, $account: String) {
               fungible_asset_metadata(where: {asset_type: {_eq: $asset_id}}) {
                 maximum_v2
@@ -93,24 +92,33 @@ export function useMintData(asset_id: string = config.asset_id) {
                 amount
               }
             }`,
-          },
-        });
+        },
+      });
 
-        const asset = res.fungible_asset_metadata[0];
-        if (!asset) return null;
+      const asset = res.fungible_asset_metadata[0];
+      if (!asset) return null;
 
-        return {
-          maxSupply: asset.maximum_v2 ?? 0,
-          currentSupply: asset.supply_v2 ?? 0,
-          uniqueHolders:
-            res.current_fungible_asset_balances_aggregate.aggregate.count ?? 0,
-          asset,
-          totalAbleToMint: await getMintLimit(asset_id),
-          yourBalance: res.current_fungible_asset_balances[0]?.amount ?? 0,
-        } satisfies MintData;
-      } catch (error) {
-        console.error(error);
-      }
+      return {
+        asset,
+        maxSupply: convertAmountFromOnChainToHumanReadable(
+          asset.maximum_v2 ?? 0,
+          asset.decimals
+        ),
+        currentSupply: convertAmountFromOnChainToHumanReadable(
+          asset.supply_v2 ?? 0,
+          asset.decimals
+        ),
+        uniqueHolders:
+          res.current_fungible_asset_balances_aggregate.aggregate.count ?? 0,
+        totalAbleToMint: convertAmountFromOnChainToHumanReadable(
+          await getMintLimit(asset_id),
+          asset.decimals
+        ),
+        yourBalance: convertAmountFromOnChainToHumanReadable(
+          res.current_fungible_asset_balances[0]?.amount ?? 0,
+          asset.decimals
+        ),
+      } satisfies MintData;
     },
   });
 }
--- templates/fungible-asset-template/frontend/utils/helpers.ts
+++ templates/fungible-asset-template/frontend/utils/helpers.ts
@@ -6,3 +6,10 @@ export const convertAmountFromHumanReadableToOnChain = (
 ) => {
   return value * Math.pow(10, decimal);
 };
+
+export const convertAmountFromOnChainToHumanReadable = (
+  value: number,
+  decimal: number
+) => {
+  return value / Math.pow(10, decimal);
+};
