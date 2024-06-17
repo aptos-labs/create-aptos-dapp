4820f8b92c15fb2ae526a27a4321b3d9dab9d509
Rasa Welcher <kwelch12@gmail.com>
2024-06-17 10:48:39 -0400

    Fixes title not appearing correctly (#87)

--- templates/digital-asset-template/frontend/components/Header.tsx
+++ templates/digital-asset-template/frontend/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useMintData } from "@/pages/Mint/hooks/useMintData";
 import { useMemo } from "react";
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 import { WalletSelector } from "./WalletSelector";
 import { IS_DEV } from "@/constants";
 import { buttonVariants } from "./ui/button";
@@ -9,18 +8,14 @@ import { config } from "@/config";
 
 export function Header() {
   const { data } = useMintData();
-  const location = useLocation();
-
-  const isRoot = location.pathname === "/";
 
   const title = useMemo(() => {
-    if (!isRoot) return "NFT Collection Launchpad";
     return (
       data?.collection.collection_name ??
       config.defaultCollection?.name ??
       "NFT Collection Launchpad"
     );
-  }, [isRoot, data?.collection]);
+  }, [data?.collection]);
 
   return (
     <div className="flex items-center justify-between px-6 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
