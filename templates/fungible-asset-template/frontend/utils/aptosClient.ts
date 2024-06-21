f71f5f70010b0f14919d878fd4092da44406f289
maayan <maayansavir@gmail.com>
2024-06-21 09:41:09 -0400

    reuse aptos client

--- templates/fungible-asset-template/frontend/utils/aptosClient.ts
+++ templates/fungible-asset-template/frontend/utils/aptosClient.ts
@@ -1,8 +1,9 @@
+import { NETWORK } from "@/constants";
 import { Aptos, AptosConfig } from "@aptos-labs/ts-sdk";
 
+const aptos = new Aptos(new AptosConfig({ network: NETWORK }));
+
+// Reuse same Aptos instance to utilize cookie based sticky routing
 export function aptosClient() {
-  const aptos = new Aptos(
-    new AptosConfig({ network: import.meta.env.VITE_APP_NETWORK })
-  );
   return aptos;
 }
