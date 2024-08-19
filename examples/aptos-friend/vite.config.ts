f49b72eb609fdecde88c497b642813587adcc3ef
J <129789810+0xaptosj@users.noreply.github.com>
2024-08-18 17:56:54 -0700

    fix build (#185)

--- examples/aptos-friend/vite.config.ts
+++ examples/aptos-friend/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   build: {
@@ -10,27 +9,10 @@ export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [
-    react(),
-    nodePolyfills({
-      exclude: ["fs"],
-      // Whether to polyfill specific globals.
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./frontend"),
-      process: "process/browser",
-      path: "path-browserify",
-      os: "os-browserify",
-      stream: "stream-browserify",
     },
   },
 });
