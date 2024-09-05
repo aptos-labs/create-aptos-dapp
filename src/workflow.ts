06fed0ac56f9f84c569ab6f9e76aefebec2f9288
aeither <36173828+aeither@users.noreply.github.com>
2024-09-05 21:10:03 +0200

    revert format

--- src/workflow.ts
+++ src/workflow.ts
@@ -5,78 +5,78 @@ import type { Result, Selections } from "./types.js";
 import { workflowOptions } from "./workflowOptions.js";
 
 export async function startWorkflow() {
-	let initialResult: Result;
+  let initialResult: Result;
 
-	try {
-		initialResult = await prompts(
-			[
-				{
-					...workflowOptions.projectName,
-					initial: "my-aptos-dapp",
-				},
-				workflowOptions.template,
-				workflowOptions.framework,
-				workflowOptions.network,
-				workflowOptions.analytics,
-			],
-			{
-				onCancel: () => {
-					throw new Error(red("✖") + " Operation cancelled");
-				},
-			},
-		);
-	} catch (err: any) {
-		console.log(err.message);
-		process.exit(0);
-	}
+  try {
+    initialResult = await prompts(
+      [
+        {
+          ...workflowOptions.projectName,
+          initial: "my-aptos-dapp",
+        },
+        workflowOptions.template,
+        workflowOptions.framework,
+        workflowOptions.network,
+        workflowOptions.analytics,
+      ],
+      {
+        onCancel: () => {
+          throw new Error(red("✖") + " Operation cancelled");
+        },
+      }
+    );
+  } catch (err: any) {
+    console.log(err.message);
+    process.exit(0);
+  }
 
-	// copy the initialResults
-	const result = { ...initialResult };
+  // copy the initialResults
+  const result = { ...initialResult };
 
-	try {
-		// A boolean variable that keeps track on whether the user wants to change their initial choices
-		let confirmOptions = true;
+  try {
+    // A boolean variable that keeps track on whether the user wants to change their initial choices
+    let confirmOptions = true;
 
-		// loop until user confirms they want to create the project
-		while (confirmOptions) {
-			// confirm prompt
-			const { confirm } = await prompts(
-				[
-					{
-						type: "confirm",
-						name: "confirm",
-						message:
-							"Do you want to make any changes to your selections? (Default is No)",
-						initial: false,
-					},
-				],
-				{
-					onCancel: () => {
-						throw new Error(red("✖") + " Operation cancelled");
-					},
-				},
-			);
+    // loop until user confirms they want to create the project
+    while (confirmOptions) {
+      // confirm prompt
+      const { confirm } = await prompts(
+        [
+          {
+            type: "confirm",
+            name: "confirm",
+            message:
+              "Do you want to make any changes to your selections? (Default is No)",
+            initial: false,
+          },
+        ],
+        {
+          onCancel: () => {
+            throw new Error(red("✖") + " Operation cancelled");
+          },
+        }
+      );
 
-			if (confirm) {
-				// a separate function for selecting the prompt you want to edit
-				await rechoseWorkflow(result);
-			} else {
-				confirmOptions = false;
-			}
-		}
-	} catch (err: any) {
-		console.log(err.message);
-		process.exit(0);
-	}
+      if (confirm) {
+        // a separate function for selecting the prompt you want to edit
+        await rechoseWorkflow(result);
+      } else {
+        confirmOptions = false;
+      }
+    }
+  } catch (err: any) {
+    console.log(err.message);
+    process.exit(0);
+  }
 
-	const { projectName, template, framework, network, telemetry } = result;
-	return {
-		projectName,
-		template:
-			template && framework === "server"
-				? { ...template, path: "nextjs-boilerplate-template" }
-				: template,
-		network,
-		telemetry,
-	} as Selections;
+  const { projectName, template, framework, network, telemetry } = result;
+  return {
+    projectName,
+    template:
+      template && framework === "server"
+        ? { ...template, path: "nextjs-boilerplate-template" }
+        : template,
+    network,
+    telemetry,
+  } as Selections;
 }
