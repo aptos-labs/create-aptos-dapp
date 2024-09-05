2497f7984f577faca5921a8d6d100902f2d0103e
aeither <36173828+aeither@users.noreply.github.com>
2024-09-05 21:47:37 +0200

    revert to fix

--- templates/nextjs-boilerplate-template/next.config.mjs
+++ templates/nextjs-boilerplate-template/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
-  basePath: "/frontend", // Sets the base path to `/some-base-path`.
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
 };
 
 export default nextConfig;
