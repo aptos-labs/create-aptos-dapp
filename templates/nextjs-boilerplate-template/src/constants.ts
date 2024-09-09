89b4bdedf79b21eb5f5ff949b67edcc91b0cfc8e
aeither <36173828+aeither@users.noreply.github.com>
2024-09-09 18:50:51 +0200

    fix next netowrk type

--- templates/nextjs-boilerplate-template/src/constants.ts
+++ templates/nextjs-boilerplate-template/src/constants.ts
@@ -1,2 +1,5 @@
-export const NETWORK = process.env.NEXT_PUBLIC_APP_NETWORK ?? "testnet";
+import type { Network } from "@aptos-labs/wallet-adapter-react";
+
+export const NETWORK: Network =
+  (process.env.NEXT_PUBLIC_APP_NETWORK as Network) ?? "testnet";
 export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
