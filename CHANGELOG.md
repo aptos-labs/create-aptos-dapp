bffb98cdd1f9e8f0914e9c116b36c97c454980cc
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-10-29 17:18:30 -0700

    support pwa in vite

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ All notable changes to the create-aptos-dapp tool will be captured in this file.
 # Unreleased
 
 - [Fix] Generate Boilerplate template without Surf errors out because of a wrong file path
+- Support PWA in the boilerplate template
 
 # 0.0.36 (2024-10-28)
 
--- templates/boilerplate-template/vite.config.ts
+++ templates/boilerplate-template/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   build: {
@@ -10,7 +11,35 @@ export default defineConfig({
     open: true,
   },
   plugins: [
-    react()
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Aptos Fullstack Template",
+        short_name: "Aptos Template",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait",
+      },
+    }),
   ],
   resolve: {
     alias: {
