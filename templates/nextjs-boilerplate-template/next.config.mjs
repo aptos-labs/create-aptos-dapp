f8b006d6bbbb24fcf1dd488b6d2aba12e735bf24
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-10-30 06:59:29 -0700

    Update next.config.mjs

--- templates/nextjs-boilerplate-template/next.config.mjs
+++ templates/nextjs-boilerplate-template/next.config.mjs
@@ -2,11 +2,6 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  // disable: process.env.NODE_ENV === "development",
-  // register: true,
-  // scope: "/app",
-  // sw: "service-worker.js",
-  //...
 });
 
 // Your Next config is automatically typed!
