39f6311472e70912d891574a8e545c66a1c4e5b7
Maayan <maayan@aptoslabs.com>
2025-05-13 14:05:44 -0400

    Remove  plugin from Boilerplate templates (#294)

--- templates/boilerplate-template/vite.config.ts
+++ templates/boilerplate-template/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   build: {
@@ -12,35 +11,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    VitePWA({
-      disable: false,
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Aptos Fullstack Template",
-        short_name: "Aptos Template",
-        icons: [
-          {
-            src: "/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "/icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        start_url: "/",
-        display: "standalone",
-        orientation: "portrait",
-      },
-    }),
   ],
   resolve: {
     alias: {
